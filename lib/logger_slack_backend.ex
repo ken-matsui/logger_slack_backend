c90104312c5e2dce3701c673bae3b03915118f5d
matken11235 <matken11235@gmail.com>
2018-06-26 08:33:52 +0900

    first commit

---
+++ lib/logger_slack_backend.ex
@@ -0,0 +1,18 @@
+defmodule LoggerSlackBackend do
+  @moduledoc """
+  Documentation for LoggerSlackBackend.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoggerSlackBackend.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
